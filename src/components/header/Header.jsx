1233565aefd7e825fced08ea67e9fe67e7c07e79
blackdoll-loki <helpmeloki7@gmail.com>
2025-03-12 17:39:12 +0200

    add burger menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,19 +2,34 @@ import React from "react";
 import List from "../list/List";
 import Button from "../button/Button";
 import '../button/button.css';
-import './header.css'
+import './header.css';
+import { useState } from "react";
 
 export default function Header(){
+    const [menuActive, setMenuActive] = useState(false); // Стан для меню
+
+    const toggleMenu = () => {
+        setMenuActive(!menuActive); // Перемикаємо стан
+    };
     const navListArr = ['Цифры', 'Сделки онлайн', 'о компании', 'как начать', 'тарифы', 'отзывы', 'FAQ'];
 
     return (
         <>
             <header>
                 <div className="logo"></div>
-                <List navList={navListArr} />
-                <div className="registration-btns">
-                    <Button btnName={"вход"} className={"btn-regular btn-transparent"}/>
-                    <Button btnName={"регистрация"} className={"btn-regular btn-blue"} />
+                <div className={`menu ${menuActive ? "active" : ""}`}>
+                    <div className={`ham-menu ${menuActive ? "active" : ""}`} 
+                        onClick={toggleMenu}
+                    >
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <List navList={navListArr} />
+                    <div className="registration-btns">
+                        <Button btnName={"вход"} className={"btn-regular btn-transparent"}/>
+                        <Button btnName={"регистрация"} className={"btn-regular btn-blue"} />
+                    </div>
                 </div>
             </header>
         </>
